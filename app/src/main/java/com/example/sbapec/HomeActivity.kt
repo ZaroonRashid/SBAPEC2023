6af3382b9231c6d293a55a2833d5db0d24674f0f
ZaroonRashid <loginwithzaroon@gmail.com>
2024-09-25 10:37:12 +0500

    SBA PEC information and web url updated

--- app/src/main/java/com/example/sbapec/HomeActivity.kt
+++ app/src/main/java/com/example/sbapec/HomeActivity.kt
@@ -1,11 +1,17 @@
 package com.example.sbapec
 
+import android.annotation.SuppressLint
 import android.app.Activity
 import android.content.Intent
 import android.content.pm.PackageManager
+import android.location.Location
+import android.location.LocationManager
 import android.net.Uri
 import android.os.Build
 import android.os.Bundle
+import android.os.Looper
+import android.provider.Settings
+import android.util.Log
 import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AlertDialog
@@ -13,15 +19,29 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import com.example.sbapec.databinding.ActivityHomeBinding
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationCallback
+import com.google.android.gms.location.LocationRequest
+import com.google.android.gms.location.LocationResult
+import com.google.android.gms.location.LocationServices
 
 
 val SCREEN_KEY = "screen"
 val SCANNER_KEY = "scanner"
 val WEB_KEY = "webUrl"
 val WEBVIEW_KEY = "webview"
+
 class HomeActivity : AppCompatActivity() {
 
     private lateinit var mBinding: ActivityHomeBinding
+    private lateinit var latitude: String
+    private lateinit var longitude: String
+
+    val LOCATION_PERMISSIONS = mutableListOf(
+        android.Manifest.permission.ACCESS_FINE_LOCATION,
+        android.Manifest.permission.ACCESS_COARSE_LOCATION
+    ).toTypedArray()
+    lateinit var mFusedLocationClient: FusedLocationProviderClient
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -35,6 +55,10 @@ class HomeActivity : AppCompatActivity() {
         mBinding.imgQr.setOnClickListener {
             checkCameraPermission()
         }
+
+        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
+        checkLocationPermission()
+
 
         mBinding.tvHelpline.setOnClickListener {
             val intent = Intent(Intent.ACTION_DIAL)
@@ -53,6 +77,15 @@ class HomeActivity : AppCompatActivity() {
                 intent.data = Uri.parse("tel:0333-4045115")
                 startActivity(intent)
             }
+        }
+    }
+
+    private fun checkLocationPermission() {
+        if (!isLocationPermGranted()) askForLocationPermission()
+        else getLastLocation { lat, lng ->
+            latitude = lat
+            longitude = lng
+            Log.d("HomeActivity", "checkLocationPermission: $lat $lng")
         }
     }
 
@@ -85,7 +118,11 @@ class HomeActivity : AppCompatActivity() {
             if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
                 openScannerActivityForResult()
             } else {
-                Toast.makeText(applicationContext, getString(R.string.permission_denied), Toast.LENGTH_SHORT).show()
+                Toast.makeText(
+                    applicationContext,
+                    getString(R.string.permission_denied),
+                    Toast.LENGTH_SHORT
+                ).show()
             }
         }
     }
@@ -102,7 +139,12 @@ class HomeActivity : AppCompatActivity() {
                 // There are no request codes
                 val data: Intent? = result.data
                 val webUrl = data?.getStringExtra(WEB_KEY)
-                showAlertDialog(webUrl)
+                if (::latitude.isInitialized && ::longitude.isInitialized)
+                    showAlertDialog("$webUrl/$latitude/$longitude")
+                else {
+                    Toast.makeText(this, "Unable to fetch location.", Toast.LENGTH_SHORT).show()
+                    showAlertDialog(webUrl)
+                }
             }
         }
 
@@ -147,5 +189,115 @@ class HomeActivity : AppCompatActivity() {
         webview.putExtra(SCREEN_KEY, WEBVIEW_KEY)
         webview.putExtra(WEB_KEY, barcodeData)
         startActivity(webview)
+    }
+
+    private fun isLocationPermGranted(): Boolean {
+        var isGranted = true
+        LOCATION_PERMISSIONS.forEach {
+            if (ContextCompat.checkSelfPermission(
+                    this, it
+                ) != PackageManager.PERMISSION_GRANTED
+            ) {
+                isGranted = false
+                return@forEach
+            }
+
+        }
+        return isGranted
+    }
+
+    private fun isLocationEnabled(): Boolean {
+        val lm = getSystemService(LOCATION_SERVICE) as LocationManager
+        var gpsEnabled = false
+        try {
+            gpsEnabled = lm.isProviderEnabled(LocationManager.GPS_PROVIDER)
+        } catch (ex: java.lang.Exception) {
+            Log.d("AppUtils", "isLocationEnabled: ${ex.message}")
+        }
+        return if (!gpsEnabled) {
+            val locationManager: LocationManager =
+                getSystemService(LOCATION_SERVICE) as LocationManager
+            locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)
+        } else gpsEnabled
+    }
+
+    private fun askForLocationPermission(
+    ) {
+        LOCATION_PERMISSIONS.forEach {
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this, it)) {
+                return@forEach
+            }
+        }
+        locationPermissionLauncher.launch(LOCATION_PERMISSIONS)
+    }
+
+    private val locationPermissionLauncher =
+        registerForActivityResult(ActivityResultContracts.RequestMultiplePermissions()) { permissions ->
+            var isGranted = true
+            LOCATION_PERMISSIONS.forEach {
+                if (permissions[it] != true) {
+                    isGranted = false
+                    return@forEach
+                }
+            }
+            if (!isGranted) {
+                Log.d(
+                    "ComplainantFormFragment", "init: Location permission not granted"
+                )
+            } else {
+                getLastLocation { lat, lng ->
+                    latitude = lat
+                    longitude = lng
+                    Log.d("HomeActivity", "$lat $lng")
+                }
+            }
+        }
+
+    @SuppressLint("MissingPermission")
+    fun getLastLocation(
+        location: (lat: String, lng: String) -> Unit
+    ) {
+        if (isLocationPermGranted()) {
+            if (isLocationEnabled()) {
+                mFusedLocationClient.lastLocation.addOnCompleteListener { task ->
+                    val location: Location? = task.result
+                    if (location == null) {
+                        requestNewLocationData(mFusedLocationClient)
+                    } else {
+                        location(location.latitude.toString(), location.longitude.toString())
+                    }
+                }
+            } else openEnableLocationPage()
+        }
+    }
+
+    @SuppressLint("MissingPermission")
+    private fun requestNewLocationData(
+        mFusedLocationClient: FusedLocationProviderClient
+    ) {
+        val mLocationRequest = LocationRequest()
+        mLocationRequest.priority = LocationRequest.PRIORITY_HIGH_ACCURACY
+        mLocationRequest.interval = 0
+        mLocationRequest.fastestInterval = 0
+        mLocationRequest.numUpdates = 1
+
+        mFusedLocationClient.requestLocationUpdates(
+            mLocationRequest, mLocationCallback, Looper.myLooper()
+        )
+    }
+
+    private fun openEnableLocationPage() {
+        val intent = Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS)
+        startActivity(intent)
+    }
+
+    private val mLocationCallback = object : LocationCallback() {
+        override fun onLocationResult(locationResult: LocationResult) {
+            locationResult.lastLocation?.let {
+                val mLastLocation: Location = it
+                var lat = mLastLocation.latitude.toString()
+                var lng = mLastLocation.longitude.toString()
+            }
+        }
     }
 }
